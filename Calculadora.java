372f4757b76c4b62b1ffbdfd37adbc76957cec7b
Isabella Vieira <isabellavieirasoares.souza@gmail.com>
2020-09-09 09:12:57 -0300

    Soma implementada. Agora implementar a subtracao.

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
 public class Calculadora{
     public static void main(String[] args) {
-        
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Primeiro operando:"));
+
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Segundo operando:"));
+
+        int res = a + b;
+        JOptionPane.showMessageDialog(null, "O resultado é: " + res);
     }
 }
